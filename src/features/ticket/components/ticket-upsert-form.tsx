5ff570a7de4e1a25bd009ebef95f909e1d553665
17etro <petrpetryk2016@gmail.com>
2024-12-30 23:13:35 +0200

    working

--- src/features/ticket/components/ticket-upsert-form.tsx
+++ src/features/ticket/components/ticket-upsert-form.tsx
@@ -1,11 +1,15 @@
 "use client";
 
 import { Ticket } from "@prisma/client";
-import { useActionState } from "react";
+import { useActionState, useRef } from "react";
 import { FieldError } from "@/components/form/field-error";
 import { Form } from "@/components/form/form";
 import { SubmitButton } from "@/components/form/submit-button";
 import { EMPTY_ACTION_STATE } from "@/components/form/utils/to-action-state";
+import {
+  DatePicker,
+  ImperativeHandleFromDatePicker,
+} from "@/components/ui/date-picker";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
@@ -16,13 +20,20 @@ interface ITicketUpsertFormProps {
 }
 
 const TicketUpsertForm = ({ ticket }: ITicketUpsertFormProps) => {
+  const datePickerImperativeHandleRef =
+    useRef<ImperativeHandleFromDatePicker>(null);
+
   const [actionState, action] = useActionState(
     upsertTicket.bind(null, ticket?.id),
     EMPTY_ACTION_STATE
   );
 
+  const handleSuccess = () => {
+    datePickerImperativeHandleRef.current?.reset();
+  };
+
   return (
-    <Form action={action} actionState={actionState}>
+    <Form action={action} actionState={actionState} onSuccess={handleSuccess}>
       <Label htmlFor="title">Title</Label>
       <Input
         id="title"
@@ -43,6 +54,22 @@ const TicketUpsertForm = ({ ticket }: ITicketUpsertFormProps) => {
         }
       />
       <FieldError actionState={actionState} name="content" />
+
+      <div className="flex gap-x-2 mb-1">
+        <div className="w-1/2">
+          <Label htmlFor="deadline">Deadline</Label>
+          <DatePicker
+            id="deadline"
+            name="deadline"
+            defaultValue={
+              (actionState.payload?.get("deadline") as string) ??
+              ticket?.deadline
+            }
+            imperativeHandleRef={datePickerImperativeHandleRef}
+          />
+          <FieldError actionState={actionState} name="deadline" />
+        </div>
+      </div>
 
       <SubmitButton label={ticket ? "Edit" : "Create"} />
 
